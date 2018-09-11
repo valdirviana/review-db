7b378029ed16c76584f95dc9f96353f0f7b7e08f
Valdir Viana <viana.valdir@gmail.com>
2018-09-11 20:59:20 -0300

    initial construction of infra

---
+++ src/ReviewDB.Infra/Data/Mapping/MovieMapping.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReviewDB.Domain.Entities.MovieAggregate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReviewDB.Infra.Data.Mapping
+{
+    public class MovieMapping : IEntityTypeConfiguration<Movie>
+    {
+        public void Configure(EntityTypeBuilder<Movie> builder)
+        {
+            builder.Property(u => u.Title).HasMaxLength(250);
+        }
+    }
+}
