ef75d5e2f63d9ebf86a90baa7a7c02221d694a11
Valdir Viana <viana.valdir@gmail.com>
2018-09-12 19:35:46 -0300

    added migration

---
+++ src/ReviewDB.Infra/Data/DesignTimeReviewDBContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace ReviewDB.Infra.Data
+{
+    public class DesignTimeReviewDBContextFactory : IDesignTimeDbContextFactory<ReviewDBContext>
+    {
+        public ReviewDBContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<ReviewDBContext>();
+            var connectionString = configuration.GetConnectionString("ReviewDBConnection");
+            builder.UseSqlServer(connectionString);
+            return new ReviewDBContext(builder.Options);
+        }
+    }
+}
