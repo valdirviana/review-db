4396978f280a731fb6e66d846bd9433b4173ab34
Valdir Viana <viana.valdir@gmail.com>
2018-09-16 16:28:50 -0300

    add reviewdb domain core

---
+++ src/ReviewDB.Domain.Core/Bus/IMediatorHandler.cs
@@ -0,0 +1,25 @@
+﻿using ReviewDB.Domain.Core.Model;
+using System;
+
+namespace ReviewDB.Domain.Core.Bus
+{
+    public class Customer : Entity
+    {
+        public Customer(Guid id, string name, string email, DateTime birthDate)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+        // Empty constructor for EF
+        protected Customer() { }
+
+        public string Name { get; private set; }
+
+        public string Email { get; private set; }
+
+        public DateTime BirthDate { get; private set; }
+    }
+}
---
+++ src/ReviewDB.Domain.Core/Commands/Command.cs
@@ -0,0 +1,19 @@
+﻿using ReviewDB.Domain.Core.Events;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReviewDB.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public abstract bool IsValid();
+    }
+}
