c615d3376b6aeb6dd1d39d9af21f0a76d044938d
Valdir Viana <viana.valdir@gmail.com>
2018-09-26 19:49:40 -0300

    start data import to import tmdb ids

---
+++ src/ReviewDB.DataImporter/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ReviewDB.DataImporter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LoadJson();
+        }
+
+        public static void LoadJson()
+        {
+            using (StreamReader r = new StreamReader("movie_ids_09_26_2018.json"))
+            {
+                string json = r.ReadToEnd();
+                List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);
+            }
+        }
+    }
+
+    public class Item
+    {
+        public bool adult;
+        public int id;
+        public string original_title;
+        public double popularity;
+        public bool video;
+    }
+}
--- src/ReviewDB.Domain/Entities/MovieAggregate/Movie.cs
+++ src/ReviewDB.Domain/Entities/MovieAggregate/Movie.cs
@@ -6,6 +6,12 @@ namespace ReviewDB.Domain.Entities.MovieAggregate
 {
     public class Movie : BaseEntity
     {
-        public string Title { get; set; }
+        public string OriginalTitle { get; set; }
+        public string Overview { get; set; }
+        public string ImdbId { get; set; }
+        public double Popularity { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Homepage { get; set; }
+        public bool Adult { get; set; }
     }
 }
